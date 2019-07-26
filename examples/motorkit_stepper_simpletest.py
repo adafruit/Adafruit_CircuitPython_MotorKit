782db333fbd2837b60de887fe7f97d6dbb7aca20
Limor "Ladyada" Fried <limor@ladyada.net>
2019-07-25 23:03:28 -0400

    Update motorkit_stepper_simpletest.py
    add delay

--- examples/motorkit_stepper_simpletest.py
+++ examples/motorkit_stepper_simpletest.py
@@ -1,7 +1,9 @@
 """Simple test for using adafruit_motorkit with a stepper motor"""
 from adafruit_motorkit import MotorKit
+import time
 
 kit = MotorKit()
 
 for i in range(100):
     kit.stepper1.onestep()
+    time.sleep(0.01)
